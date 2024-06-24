a84e7527b267e84dc4899028c8384ddce945980c
Samar082 <mahatosamar479@gmail.com>
2024-06-25 01:26:16 +0530

    datatypes conversion

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+//Datatypes
+
+//number => 2 to the power 53
+//bigint
+//string => ""
+//boolean => true or false
+//null  => standalone value
+//undefined
+//symbol => unique
+
+let score = 3
+let stringScore = String(score)
+console.log(stringScore);
+console.log(typeof(stringScore));
+
+let Isboolean = "c"
+let booleanValue = Boolean(Isboolean)
+console.log(booleanValue);
+console.log(typeof(booleanValue));
+
+let marks = null
+let stringMarks = Boolean(marks)
+console.log(typeof(marks));
+console.log(stringMarks);
