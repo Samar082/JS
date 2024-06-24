ab4dc800dbaea109d3cd9035286f4331a05c623e
Samar082 <mahatosamar479@gmail.com>
2024-06-24 11:45:33 +0530

    Initial commit

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12267
+let accountEmail = "fghcxj124@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState;
+
+// accountId = 2 <== You cannot change ==>
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212122"
+accountCity = "Durgapur"
+
+console.log(accountId)
+
+/*
+Prefer not to use var
+because og issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,1 @@
-console.log("Samar")
+console.log("Samar");
