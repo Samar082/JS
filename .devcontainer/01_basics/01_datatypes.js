511c3dc33f1a6661028b2c6443534879fdbedfc9
Samar082 <mahatosamar479@gmail.com>
2024-06-26 05:40:46 +0530

    operations

--- .devcontainer/01_basics/01_datatypes.js
+++ .devcontainer/01_basics/01_datatypes.js
@@ -10,26 +10,59 @@
 //undefined
 //symbol => unique
 
-let score = 3
-let stringScore = String(score)
-console.log(stringScore);
-console.log(typeof(stringScore));
+// let score = 3
+// let stringScore = String(score)
+// console.log(stringScore);
+// console.log(typeof(stringScore));
 
-let Isboolean = "c"
-let booleanValue = Boolean(Isboolean)
-console.log(booleanValue);
-console.log(typeof(booleanValue));
+// let Isboolean = "c"
+// let booleanValue = Boolean(Isboolean)
+// console.log(booleanValue);
+// console.log(typeof(booleanValue));
 
-let marks = null
-let stringMarks = Boolean(marks)
-console.log(typeof(marks));
-console.log(stringMarks);
+// let marks = null
+// let stringMarks = Boolean(marks)
+// console.log(typeof(marks));
+// console.log(stringMarks);
 
-let num = "33abc"
-let numValue = Number(num)
-console.log(numValue);
-console.log(typeof(numValue));
+// let num = "33abc"
+// let numValue = Number(num)
+// console.log(numValue);
+// console.log(typeof(numValue));
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
+
+// ***************** Operations *****************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " Samar"
+
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + "2" + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
